a59166f867ee2bfb80244c3bfd06733f28054448
jimmyboy112 <guanj37@gmail.com>
2025-02-14 10:09:40 -0800

    Add files via upload

---
+++ somethingGood/index.js
@@ -0,0 +1,9 @@
+function submit(){
+    const a = Number(document.querySelector('.s2').value);
+    const b = Number(document.querySelector('.index').value);
+   
+    let c = a+b;
+
+    document.querySelector('.text-a').value= c;
+}
+
